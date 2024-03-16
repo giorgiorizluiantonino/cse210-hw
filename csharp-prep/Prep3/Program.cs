963f511c2339c10285fe1e279302ff75b80884c7
giorgiorizluiantonino <grizluiantonino@gmail.com>
2024-03-17 01:53:58 +0800

    done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomgenerator = new Random();
+        int magic_number  = randomgenerator.Next(1,10);
+
+        int guess = -1;
+       
+
+        
+        while (guess !=  magic_number){
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (magic_number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (magic_number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+
+        }
+        
+
     }
 }
