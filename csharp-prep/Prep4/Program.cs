b6fb3ac7c616b95866fe710e2b08cb2638bc36c6
giorgiorizluiantonino <grizluiantonino@gmail.com>
2024-03-17 02:01:22 +0800

    done 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,50 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+     {
+        List<int> numbers = new List<int>();
+        
+        int user_num = -1;
+        while (user_num != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string response = Console.ReadLine();
+            user_num = int.Parse(response);
+            
+           
+            if (user_num != 0)
+            {
+                numbers.Add(user_num);
+            }
+        }
+
+      
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+      
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
